735a02f24af642cea1b99d75d59061beb0e7ddc5
haxom <haxom@haxom.net>
2013-11-22 18:19:46 +0100

    alpha powa

--- bruty.py
+++ bruty.py
@@ -3,35 +3,33 @@
 __author__	= "haxom"
 __email__	= "haxom@haxom.net"
 __file__	= "bruty.py"
-__version__	= "0.2"
+__version__	= "0.3"
 
 ## Imports ##
 from optparse import OptionParser
 from os import path
 import sys
 import curses
-#from subprocess import check_output as call
-from subprocess import Popen as call
-from subprocess import STDOUT
-from subprocess import PIPE
 from threading import Thread, active_count
 import Queue
+import httplib2
 
-## Globals ##
-waitingString = ('-', '\\', '|', '/')
+## Defaults ##
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0'}
 
 ## Params ##
 def getParams():
 	parser = OptionParser()
-	parser.add_option('', '--files', dest='filenames', default='', help='PATH to filenames wordilist')
-	parser.add_option('', '--directories', dest='directories', default='',  help='PATH to directories wordlist')
-	parser.add_option('', '--patator', dest='patator', default='', help='PATH to patator.py script')
-	parser.add_option('', '--url', dest='url', default='http://localhost', help='based URL (without FILEX text)')
-	parser.add_option('', '--options', dest='options', default='', help='options provided to patator.py')
-	parser.add_option('', '--log', dest='log', default=False, action='store_true', help='enable log of patator (-l) with dynamic name destination')
+	parser.add_option('-f', '--files', dest='filenames', default='', help='PATH to filenames wordilist')
+	parser.add_option('-d', '--directories', dest='directories', default='',  help='PATH to directories wordlist')
+	parser.add_option('-u', '--url', dest='url', default='http://localhost', help='based URL (without FILEX text)')
+	parser.add_option('-H', '--headers', dest='headers', default='', help='custom the headers (name:value;name:value...)')
+	parser.add_option('-l', '--log', dest='log', default=False, action='store_true', help='log the requests/responses')
+	parser.add_option('-x', '--exclude', dest='exclude', default='404', help='exclude return code, separated by a coma')
 	parser.add_option('', '--level', dest='level', default=-1, help='subdirectories max. level (< 0 = infinite)')
-	global options
+	parser.add_option('-m', '--method', dest='method', default='GET', help='HTTP method (default: GET)')
 	(options, args) = parser.parse_args(sys.argv)
+	return options
 
 def display(x, y, text):
 	stdscr.addstr(x, y, text)
@@ -39,27 +37,37 @@ def display(x, y, text):
 
 def drawTree(tree):
 	display(13, 0, tree)
+	# ToDo
 
-def execCall(options, rest, queue):
-	cmd = 'python %s http_fuzz %s url=%s/FILE0 0=%s' % (options.patator, options.options, options.url, rest)
-	a = call([cmd], shell=True, stdout=PIPE, stderr=STDOUT)
-	stdout, stderr = a.communicate()
-	queue.put(stdout)
+def search(options, dict, queue):
+	http =  httplib2.Http()
+	positiv = list()
+	for current in dict:
+		response, content = http.request('%s/%s' % (options.url, current), options.method, headers=headers)
+		if str(response.status) not in options.exclude:
+			positiv.append(current)
+	queue.put(positiv)
 
 ## Main ##
 if __name__ == '__main__':
-	getParams()
+	options = getParams()
 	options.level = int(options.level)
 	if len(options.filenames) <= 0 or not path.exists(options.filenames):
 		options.filenames = ''
 	if len(options.directories) <= 0 or not path.exists(options.directories):
 		options.directories = ''
+	options.exclude = options.exclude.split(',')
+	if len(options.headers) != 0:
+		headers = dict()
+		for entry in options.headers.split(';'):
+			nv = entry.split(':')
+			if nv[0].lower() in 'User-Agent':
+				headers.update({'User-Agent':nv[1]})
+			else:
+				headers.update({nv[0]:nv[1]})
 
 	if options.directories == '' and options.filenames == '':
 		print 'Both --files and --directories are empty or invalid !'
-		sys.exit()
-	if not path.exists(options.patator):
-		print '--patator is empty or invalid !'
 		sys.exit()
 
 	stdscr = curses.initscr()
@@ -78,33 +86,30 @@ if __name__ == '__main__':
 	display(10, 0, '| Directories based on: %s' % options.directories)
 	display(11, 0, '| Level of subdirectories scanned: %d' % options.level)
 
-	results = dict()
-
+	display(13, 0, '| Building ...')
 	try:
 
 		cur_lvl = 0
+		queue = Queue.Queue()
 		while cur_lvl <= options.level or options.level < 0:
 			# files
-			queue = Queue.Queue()
-			t1 = Thread(target=execCall, args=(options, options.filenames, queue), kwargs=None)
+			f = open(options.filenames, 'r')
+			currents = list()
+			for current in f:
+				currents.append(current)
+			f.close()
+
+			t1 = Thread(target=search, args=(options, currents, queue), kwargs=None)
 			t1.setDaemon(True)
 			t1.start()
 
-			c = 0
-			display(13, 0, waitingString[c%4])
 			while active_count() > 2:
-				c+=1
-				t2 = Thread(target=display, args=(13, 0, waitingString[c%4]), kwargs=None)
-				t2.setDaemon(True)
-				t2.start()
-				t2.join()
 				sleep(0.5)
 
-			drawTree(queue.get())
+			l = len(queue.get())
+			display(14, 0, '%d found' % l)
 
 			# directories
-			#cmd = 'python %s http_fuzz %s url=%s/FILE0 0=%s' % (options.patator, options.options, options.url, options.directories)	
-
 			cur_lvl+=1
 
 	except Exception as e:
