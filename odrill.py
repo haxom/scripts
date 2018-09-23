118e14c6255ac9a93ac225b3250c8796eb9c3a62
haxom <haxom@haxom.net>
2018-09-24 00:47:24 +0200

    web interface + web search email + web search from file

--- odrill.py
+++ odrill.py
@@ -32,8 +32,13 @@ import httplib2
 import urllib
 import json
 import operator
+import binascii
+
+# web interface
+import BaseHTTPServer
 import SocketServer
-import SimpleHTTPServer
+import urlparse
+import urllib
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0'}
 data_folder = '/tmp/'
@@ -54,6 +59,7 @@ def get_params():
 	(options, args) = parser.parse_args(sys.argv)
 	return options
 
+## {{ Printers methods
 def debug(message):
     if options.debug:
         print '[debug] %s' % message
@@ -63,11 +69,41 @@ def output(message):
     if options.output != '':
         options.output_handler.write(message)
         f.close()
+## }}
         
+## {{ Int. methods
+def search_one(email):
+    results_output = dict()
+    results = search_email(email)
+    results = results['rows']
+    if results[0] == {}:
+        # no result
+        results_output[email] = {'count':0, 0:{'rm':'', 'date':'', 'pass':'', 'entite':''}}
+    else:
+        results_output[email] = {'count':len(results)}
+        for i in range(len(results)):
+            results_output[email][i] = {'rm':results[i]['RM'], 'date':results[i]['Date'], 'entite':results[i]['Entite'], 'pass':results[i]['Password']}
+    return results_output
+
+def search_file(f):
+    results_output = dict()
+    with open(f, 'r') as fp:
+        for cnt, line in enumerate(fp):
+            line = line.rstrip()
+            results = search_email(line)
+            results = results['rows']
+            if results[0] == {}:
+                # no result
+                results_output[line] = {'count':0, 0:{'rm':'', 'date':'', 'pass':''}}
+            else:
+                results_output[line] = {'count':len(results)}
+                for i in range(len(results)):
+                    results_output[line][i] = {'rm':results[i]['RM'], 'date':results[i]['Date'], 'entite':results[i]['Entite'], 'pass':results[i]['Password']}
+    return results_output
 
 def search_email(email):
     debug('Searching email : %s'%email)
-    request = 'SELECT * FROM dfs.`%s/*.parquet` WHERE LOWER(Mail) LIKE LOWER(\'%%%s%%\') ORDER BY `RM` ASC' % (data_folder, email)
+    request = 'SELECT * FROM dfs.`%s/*.json` WHERE LOWER(Mail) LIKE LOWER(\'%%%s%%\') ORDER BY `RM` ASC' % (data_folder, email)
     debug(request)
     resp = send_request(request)
     return resp
@@ -82,16 +118,163 @@ def send_request(request):
 
     return json.loads(data)
 
+## }}
+
+## {{ Console
 
 def console():
     print '## ToDo : console ##'
 
-def webserver():
-    handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(('127.0.0.1', 8888), handler)
-    print 'Web server launched. Listenning on 127.0.0.1:8888'
-    httpd.serve_forever()
+## }}
 
+## {{ Web Server
+class WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
+    is_post = False
+
+    def set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+
+    def html_headers(self):
+        self.wfile.write('<html>\n<head>\n')
+        self.wfile.write('<style>\n')
+        self.wfile.write('body {margin: 10; font-family: "Merriweather", sans-serif; font-size: 1rem; font-weight: 400; line-height: 1.9; color: #999; text-align: left; background-color: #fff;}\n')
+        self.wfile.write('hr {box-sizing:  content-box; height: 0; overflow: visible;}')
+        self.wfile.write('h1, h2, h3, h4, h5, h6 {margin-top: 0; margin-bottom: 1rem; color: #333}')
+        self.wfile.write('p {margin-top: 0; margin-bottom: 1rem;}')
+        self.wfile.write('hr {box-sizing: content-box; height: 0; overflow: visible;}')
+        self.wfile.write('img {vertical-align: middle; border-style: none;}')
+        self.wfile.write('table {border-collapse: collapse;}')
+        self.wfile.write('label {display: inline-block; margin-bottom: .5rem;}')
+        self.wfile.write('button {border-radius: 0;}')
+        self.wfile.write('button:focus {outline: 1px dotted; outline: 5px auto -webkit-focus-ring-color;}')
+        self.wfile.write('input, button, select, optgroup, textarea {margin: 0; font-family: inherit; font-size: inherit; line-height: inherit;}')
+        self.wfile.write('button, input {overflow: visible;}')
+        self.wfile.write('textarea {overflow: auto; resize: vertical;}')
+        self.wfile.write('code {padding: 0.2rem 0.4rem; font-size: 90%; color: #bd4147; background-color: #f8f9fa; border-radius: 0px;}')
+        self.wfile.write('.navbar-text {display: inline-block; padding-top: 0.5rem; padding-bottom: 0.5rem;}')
+        self.wfile.write('.navbar-collapse {flex-basis: 100%; flex-grow: 1; align-items: center;}')
+        self.wfile.write('.navbar-light, .navbar-nav, .navbar-link, .dropdown-item {color: #fff; background-color: #acacac; font-weight: bold;  padding: 1rem 2rem !important; font-size: 0.9rem;}')
+        self.wfile.write('.btn {display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; user-select: none; border: 1px solid transparent; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.9; border-radius: 0px; transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out; }')
+        self.wfile.write('.btn-primary {color: #fff; background-color: #333; border-color: #333;}')
+        self.wfile.write('.btn-primary:hover {color: #fff; background-color: #202020; border-color: #1a1a1a;}')
+        self.wfile.write('.btn-primary:focus {box-shadow: 0 0 0 0.2rem rgba(51, 51, 51, 0.5);}')
+        self.wfile.write('.btn-primary:disabled {background-color: #333; border-color: #333;}')
+        self.wfile.write('.btn-primary:not {color: #fff; background-color: #1a1a1a; border-color: #131313; box-shadow: 0 0 0 0.2rem rgba(51, 51, 51, 0.5);}')
+        self.wfile.write('.footer-lists {background-color: #ebebeb; padding: 2rem; margin-bottom: 1rem; border-top: 1px solid #d2d2d2; font-size: 0.8rem}')
+        self.wfile.write('.footer-lists h4 {color #999999}')
+        self.wfile.write('hr {box-sizing: content-box; height: 0; overflow: visible;}')
+        self.wfile.write('</style>\n')
+        self.wfile.write('</head>\n<body>\n')
+
+
+    def html_footers(self):
+        self.wfile.write('</body>\n</html>\n')
+
+    def do_GET(self):
+        parsed_path = urlparse.urlparse(self.path)
+        params_tmp = parsed_path.query.split(';')
+        params = list()
+        for p in params_tmp:
+            params.append(p.split('='))
+
+        self.set_headers()
+        self.html_headers()
+        self.wfile.write('\n')
+
+        # title
+        self.wfile.write('<center><h1>ODRILL v%s</h1></center>\n'%__version__)
+
+        # top bar
+        self.wfile.write('<div class="collapse navbar-collapse"><ul class="navbar-nav mr-auto">\n<center>')
+        self.wfile.write('<form method="get" action="odrill.py" style="display: inline;">\n')
+        self.wfile.write('<input size="25" placeholder="bla@bla.com" type="text" name="search_term"> ')
+        self.wfile.write('<button class="btn btn-primary" type="submit">recherche</button></form>\n | \n')
+        self.wfile.write('<form enctype="multipart/form-data" method="post" action="odrill.py" style="display: inline;">\n')
+        self.wfile.write('<input size="10" type="file" name="search_term_file"> ')
+        self.wfile.write('<button class="btn btn-primary" type="submit">envoi et recherche</button></form>\n')
+        self.wfile.write('</center></ul></div>')
+
+        # results
+        self.wfile.write('<div class="footer-lists">\n')
+        self.wfile.write('<center><h4>Résultats</h4></center>\n')
+        self.wfile.write('<hr>')
+        
+        # only one term
+        for p in params:
+            if 'search_term' in p:
+                search_term = urllib.unquote(p[1])
+                self.wfile.write('Terme recherché : %s\n' % search_term)
+                self.wfile.write('<hr>\n')
+                results = search_one(search_term)
+                results = sorted(results.items(), key=operator.itemgetter(1))
+                results.reverse()
+                for r in results:
+                    if r[1]['count'] == 0:
+                        self.wfile.write('<b><i>Aucun résultat</i></b><br />\n')
+                    for i in range(r[1]['count']):
+                        if options.password:
+                            self.wfile.write('  - RM%s (%s / %s) : %s<br />\n'%(r[1][i]['rm'], r[1][i]['date'], r[1][i]['entite'], r[1][i]['pass']))
+                        else:
+                            self.wfile.write('  - RM%s (%s / %s)<br />\n'%(r[1][i]['rm'], r[1][i]['entite'], r[1][i]['date']))
+
+        # file-terms
+        if self.is_post:
+            data_len = int(self.headers['Content-Length'])
+            read_len = 0
+            data_bound = self.rfile.readline() + self.rfile.readline() + self.rfile.readline() + self.rfile.readline()
+            read_len += len(data_bound)
+            data_bound_s = data_bound.split('\n')[0][:-1]
+            filename = data_bound[data_bound.find('filename')+8+2:data_bound.find('"', data_bound.find('filename')+8+2+1)]
+
+            f = open('%s/odrill_upload.txt'%data_folder, 'w')
+            count = 0
+            before = self.rfile.readline()
+            data = self.rfile.readline()
+            read_len += len(before) + len(data)
+            while read_len < data_len:
+                count += 1
+                f.write(before)
+                before = data
+                data = self.rfile.readline()
+                read_len += len(data)
+            f.close()
+            self.wfile.write('Fichier : %s (%d entrées) <br />\n' % (filename, count))
+
+            results = search_file('%s/odrill_upload.txt'%data_folder)
+            results = sorted(results.items(), key=operator.itemgetter(1))
+            results.reverse()
+            for r in results:
+                self.wfile.write('* %s<br />\n' % r[0])
+                if r[1]['count'] == 0:
+                    self.wfile.write('<b><i>Aucun résultat</i></b><br />\n')
+                for i in range(r[1]['count']):
+                    if options.password:
+                        self.wfile.write('  - RM%s (%s / %s) : %s<br />\n'%(r[1][i]['rm'], r[1][i]['date'], r[1][i]['entite'], r[1][i]['pass']))
+                    else:
+                        self.wfile.write('  - RM%s (%s / %s)<br />\n'%(r[1][i]['rm'], r[1][i]['entite'], r[1][i]['date']))
+
+        self.wfile.write('<hr>')
+        self.wfile.write('</div>')
+
+
+        self.wfile.write('\n')
+        self.html_footers()
+        self.is_post = False
+
+    def do_POST(self):
+        self.is_post = True
+        self.do_GET()
+
+def run_webserver():
+    server = BaseHTTPServer.HTTPServer
+    handler = WebServer
+    print 'Web server launched. Listenning on 127.0.0.1:8888'
+    httpd = server(('127.0.0.1', 8888), handler)
+    httpd.serve_forever()
+## }}
 
 ## Main ##
 if __name__ == '__main__':
@@ -107,7 +290,7 @@ if __name__ == '__main__':
 
         if options.web:
             debug('launching web server...')
-            webserver()
+            run_webserver()
             sys.exit()
 
         if options.console:
@@ -116,46 +299,28 @@ if __name__ == '__main__':
             sys.exit()
 
         # results_output will store all results
+        results_output_one = dict()
         results_output = dict()
-
+        
         if options.email != '':
             debug('processing "email" option')
-            results = search_email(options.email)
-            results = results['rows']
-            if results[0] == {}:
-                # no result
-                results_output[options.email] = {'count':0, 0:{'rm':'', 'date':'', 'pass':'', 'entite':''}}
-            else:
-                results_output[options.email] = {'count':len(results)}
-                for i in range(len(results)):
-                    results_output[options.email][i] = {'rm':results[i]['RM'], 'date':results[i]['Date'], 'entite':results[i]['Entite'], 'pass':results[i]['Password']}
+            results_output_one = search_one(options.email)
 
         if options.file != '':
             debug('processing "file" option')
             debug('opening file %s'%options.file)
+            results_output = search_file(options.file)
 
-
-            with open(options.file, 'r') as fp:
-                for cnt, line in enumerate(fp):
-                    line = line.rstrip()
-                    results = search_email(line)
-                    results = results['rows']
-                    if results[0] == {}:
-                        # no result
-                        results_output[line] = {'count':0, 0:{'rm':'', 'date':'', 'pass':''}}
-                    else:
-                        results_output[line] = {'count':len(results)}
-                        for i in range(len(results)):
-                            results_output[line][i] = {'rm':results[i]['RM'], 'date':results[i]['Date'], 'entite':results[i]['Entite'], 'pass':results[i]['Password']}
+        results = results_output_one
+        results.update(results_output)
 
         # print results
-        results_output = sorted(results_output.items(), key=operator.itemgetter(1))
-        results_output.reverse()
-        for r in results_output:
+        results = sorted(results.items(), key=operator.itemgetter(1))
+        results.reverse()
+        for r in results:
             print '* %s' % r[0]
             for i in range(r[1]['count']):
                 if options.password:
                     print '  - RM%s (%s / %s) : %s'%(r[1][i]['rm'], r[1][i]['date'], r[1][i]['entite'], r[1][i]['pass'])
                 else:
                     print '  - RM%s (%s / %s)'%(r[1][i]['rm'], r[1][i]['entite'], r[1][i]['date'])
-
